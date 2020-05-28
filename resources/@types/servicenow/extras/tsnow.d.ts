ddc62285b8f5d3e931ca5f20e7408b45b00eb728
Martin Chovanec <chovanecm@gmail.com>
2020-05-28 16:37:22 +0200

    Introduce "ObjectOfStrings" and "Object" the first one used to represent objects read from a GlideRecord, the other one used to represent objects merged to a GlideRecord

--- resources/@types/servicenow/extras/tsnow.d.ts
+++ resources/@types/servicenow/extras/tsnow.d.ts
@@ -1,6 +1,33 @@
 declare namespace tsnow {
-    type FieldsOf<T> = internal.OwnKeys<T>
-    type Object<T> = {
-        [key in FieldsOf<T>]: internal.ValueDomainByElement<T[key]>
+    /**
+     * string, number or boolean
+     */
+    type primitive = internal.primitive
+    /**
+     * Any field name of the given table
+     */
+    type FieldsOf<T> = internal.OwnKeys<T extends TableName ? TableIndex[T] : T>
+    /**
+     * Any valid query key (e.g. sys_id, parent.sys_id) of the given table
+     *
+     * Note.: Due to implementation details, only a limited number of dot-walking (.) steps is supported.
+     */
+    type QueryKeys<T> = internal.QueryKeys<T extends TableName ? TableIndex[T] : T>
+    /**
+     * An object representing the GlideRecord {field1: value1, field2: value2, ...}
+      * where are values are guaranteed to be strings (i.e. retrieved using "getValue()")
+     */
+    type ObjectOfStrings<T> = {
+        [key in FieldsOf<T>]: string
     }
+    /**
+     * An object representing the GlideRecord. Similar to {@link ObjectOfStrings} but the values can be strings, numbers or boolean
+     */
+    type Object<T> = {
+        [key in FieldsOf<T>]: primitive
+    }
+    /**
+     * An allowed Query operator (IN, =, !=, etc.)
+     */
+    type QueryOperator = internal.QueryOperator
 }
